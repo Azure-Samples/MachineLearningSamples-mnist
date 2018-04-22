0010272408af644557eba726d9d1857e884769e1
Hai Ning <mit_hai@hotmail.com>
2018-04-21 23:43:35 -0400

    plot

--- mnist_sklearn.py
+++ mnist_sklearn.py
@@ -2,10 +2,17 @@ from sklearn.datasets import fetch_mldata
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import numpy as np
-from azureml.core.run import Run
 import os
 
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+from azureml.core.run import Run
+from PlotUtils import plot_digits
+
 data_path = '/tmp'
+os.makedirs('./outputs', exist_ok=True)
     
 if "AZUREML_NATIVE_SHARE_DIRECTORY" in os.environ:
     print('use shared folder:')
@@ -20,8 +27,15 @@ run = Run.get_submitted_run()
 print('fetching MNIST data...')
 mnist = fetch_mldata('MNIST original', data_home=data_path)
 
-# use the full set with 70k records
-#X, y = mnist['data'], mnist['target']
+# use the full set with 70,000 records
+X, y = mnist['data'], mnist['target']
+
+print('plot some digits...')
+plt.figure(figsize=(9,9))
+example_images = np.r_[X[:12000:600], X[13000:30600:600], X[30600:60000:590]]
+plot_digits(example_images, images_per_row=10)
+plt.show()
+plt.savefig('./outputs/digits.png', format='png', dpi=300)
 
 # use a random subset of n records to reduce training time.
 n = 5000
